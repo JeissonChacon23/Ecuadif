8f010530b667b43bc19e849bfa47cf14f10ab45b
JeissonChacon23 <jeissonchaconjp@gmail.com>
2024-12-05 10:58:28 -0500

    Content View

---
+++ Ecuadif/Ecuadif/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ecuadif
+//
+//  Created by Jeisson Chacón on 5/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ecuadif/Ecuadif/EcuadifApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcuadifApp.swift
+//  Ecuadif
+//
+//  Created by Jeisson Chacón on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EcuadifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
